74434426cf22804054f744505e85a0631cb4a62d
cryptax <aafortinet@gmail.com>
2018-06-05 10:41:53 +0200

    fix for python 2.7

--- eviction_strategy_evaluator/eviction_strategy_evaluator/utils.py
+++ eviction_strategy_evaluator/eviction_strategy_evaluator/utils.py
@@ -9,9 +9,12 @@ def execute_command(command):
     proc = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     debug = True
     if debug is True:
-        for l in io.TextIOWrapper(proc.stdout, encoding='utf-8'):
-            print(l, end="")
-        for l in io.TextIOWrapper(proc.stderr, encoding='utf-8'):
-            print(l, end="")
+        my_stdout = io.open(proc.stdout.fileno(), mode='r', encoding="utf-8")
+        for l in my_stdout:
+            print(l)
+        my_stderr = io.open(proc.stderr.fileno(), mode='r', encoding="utf-8")
+        for l in my_stderr:
+        #for l in io.TextIOWrapper(my_stderr, encoding='utf-8'):
+            print(l)
     else:
         proc.communicate()
